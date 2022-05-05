28275a826b58ac9b2f2761b958806ce0e86a8843
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-05-04 23:47:25 -0600

    se agrego handleClick a la clase Board.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-
 class Square extends React.Component {
     render() {
       return (
@@ -23,11 +18,16 @@ class Square extends React.Component {
         squares:Array(9).fill(null),
       };
     }
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
     renderSquare(i) {
       return( 
       <Square 
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
         />
       );
     }
