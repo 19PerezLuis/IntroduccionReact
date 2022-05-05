ec8674d8aa9dc8909e4a693e5de7de4a9987356e
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-05-05 12:21:38 -0600

    Cambios para declarar el ganador.

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ function Square(props) {
       this.setState({
         squares: squares,
         xIsnext: !this.state.xIsnext,
-
       });
     }
     renderSquare(i) {
@@ -31,10 +30,14 @@ function Square(props) {
         />
       );
     }
-  
     render() {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
       const status = 'Next player: X'+(this.setState.xIsnext ? 'X' : 'O');
-  
+      }
       return (
         <div>
           <div className="status">{status}</div>
@@ -78,4 +81,24 @@ function Square(props) {
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
