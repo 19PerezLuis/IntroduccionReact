32f88f15a54e247ec6a95a05f601503a8d40131a
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-05-04 22:56:32 -0600

    estamos pasando dos props desde Board a Square

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+
 class Square extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        value: null,
-      };  
-    }
     render() {
       return (
-        <button className="square" onClick={()=>this.setState({value:'X'})}>
+        <button 
+          className="square"
+          onClick={() => this.props.onClick()}
+         >
           {this.props.value}
         </button>
       );
